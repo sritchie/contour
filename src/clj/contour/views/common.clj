0d3d73efc55bb18df220b84b4aa0630fb0fce653
Sam Ritchie <sritchie09@gmail.com>
2011-11-27 07:50:21 -0500

    Added meta tag for utf-8.

--- src/clj/contour/views/common.clj
+++ src/clj/contour/views/common.clj
@@ -5,9 +5,10 @@
 (defpartial layout [& content]
   (html5
    [:head
-    [:meta
-     {:name "viewport"
-      :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
+    [:meta {:name "viewport"
+            :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
+    [:meta {:content "text/html;charset=UTF-8"
+            :http-equiv "content-type"}]
     [:title "Contour"]
     (include-css "/css/reset.css")]
    [:body content]))
